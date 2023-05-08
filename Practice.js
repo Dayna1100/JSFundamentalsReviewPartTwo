620c6a8bb5f626f11e002cf76f7d67e933b47a5a
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-05-08 16:37:52 -0400

    initial commit

---
+++ Practice.js
@@ -0,0 +1,17 @@
+//1. Create a function named `greeting` that prints the "Hello World" to the console.
+
+// function greeting() {
+//     console.log('Hello World');
+// }
+
+// greeting();
+
+// 2. Create a function named `countingCharacters` that takes a `string` as a parameter.
+//     - If the length of the string is less than 10, print the strings length to the 
+//console 
+//     - If the string length is more than 10, print a message that the string is too 
+//long and include the string in the message.
+
+function countingCharacters() {
+
+}
